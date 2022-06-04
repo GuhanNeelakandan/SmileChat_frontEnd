16e7b870aceb0aed9c656569e33e4c348ed856fe
Guhan <you@example.com>
2022-06-04 21:52:04 +0530

    changes

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,17 +1,36 @@
 import React, { useState } from 'react'
 import { IoMdSend } from "react-icons/io";
-function ChatInput({handleSendMsg}) {
+import { BsEmojiSmileFill } from "react-icons/bs";
+import Picker from "emoji-picker-react";
+
+
+function ChatInput({ handleSendMsg }) {
     const [msg, setMsg] = useState("");
+    const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+
+    const handleEmojiPickerhideShow = () => {
+        setShowEmojiPicker(!showEmojiPicker);
+    };
+
+    const handleEmojiClick = (event, emojiObject) => {
+        let message = msg;
+        message += emojiObject.emoji;
+        setMsg(message);
+    };
     const sendChat = (event) => {
         event.preventDefault();
         if (msg.length > 0) {
-          handleSendMsg(msg);
-          setMsg("");
+            handleSendMsg(msg);
+            setMsg("");
         }
-      };
+    };
     return (
         <>
             <form className='type-container' onSubmit={(event) => sendChat(event)}>
+                <div className="emoji">
+                    <BsEmojiSmileFill onClick={handleEmojiPickerhideShow} />
+                    {showEmojiPicker && <Picker onEmojiClick={handleEmojiClick} />}
+                </div>
                 <input type="text" name='message' placeholder='Type Your Message' onChange={(e) => setMsg(e.target.value)}
                     value={msg} />
                 <button className='submit'>
